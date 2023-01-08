9d958912bb8d09b283fd2f805d7596e25b9a2eb4
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-08 21:30:35 +0800

    调整样式

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,24 @@ type PositionType = {
   y: number;
 };
 
+/**
+ * 存储位置信息
+ */
+const positionProxy = new Proxy(
+  {
+    x: 0,
+    y: 0
+  },
+  {
+    set(target: PositionType, p: string | symbol, newValue: any, receiver: any) {
+      return Reflect.set(target, p, newValue, receiver);
+    },
+    get(target: PositionType, p: string | symbol, receiver: any) {
+      return Reflect.get(target, p, receiver);
+    }
+  }
+);
+
 window.addEventListener("load", (): void => {
   const selectorArea = document.querySelector(".selector-area") as HTMLDivElement;
   const selectorBox = document.querySelector(".selector-box") as HTMLDivElement;
@@ -14,104 +32,8 @@ window.addEventListener("load", (): void => {
   const allSpans: Array<HTMLSpanElement> = [];
   const selectedSpanBucket: Set<HTMLSpanElement> = new Set<HTMLSpanElement>();
 
-  // 填充元素
-  const fragmentWrapper: DocumentFragment = document.createDocumentFragment();
-  for (let i: number = 1; i <= 36; ++i) {
-    const span = document.createElement("span");
-    span.classList.add("item");
-    span.textContent = `${i}`;
-    allSpans.push(span);
-    fragmentWrapper.appendChild(span);
-  }
-  // 设置代理绑定监听器
-  contentArea.addEventListener("click", (ev) => {
-    ev.preventDefault();
-    ev.stopImmediatePropagation();
-    ev.stopPropagation();
-    selectedSpanBucket.clear();
-    if (ev.target !== contentArea) {
-      (ev.target as HTMLSpanElement).classList.toggle("when-selected");
-      selectedSpanBucket.add(ev.target as HTMLSpanElement);
-    }
-  });
-  contentArea.appendChild(fragmentWrapper);
-
-  /**
-   * 存储位置信息
-   */
-  const positionProxy = new Proxy(
-    {
-      x: 0,
-      y: 0
-    },
-    {
-      set(target: PositionType, p: string | symbol, newValue: any, receiver: any) {
-        return Reflect.set(target, p, newValue, receiver);
-      },
-      get(target: PositionType, p: string | symbol, receiver: any) {
-        return Reflect.get(target, p, receiver);
-      }
-    }
-  );
-
-  document.addEventListener("mousedown", (clickEv: MouseEvent): void => {
-    clickEv.stopPropagation();
-    positionProxy.x = clickEv.pageX - selectorArea.offsetLeft;
-    positionProxy.y = clickEv.pageY - selectorArea.offsetTop;
-    if (positionProxy.x >= 0 && positionProxy.y >= 0) {
-      document.addEventListener("mousemove", whenMoveAction);
-    }
-  });
-
-  document.addEventListener("mouseup", (): void => {
-    document.removeEventListener("mousemove", whenMoveAction);
-
-    const startLeft: number = selectorBox.offsetLeft;
-    const startTop: number = selectorBox.offsetTop;
-    const selectorEndWidth: number = startLeft + selectorBox.offsetWidth;
-    const selectorEndHeight: number = startTop + selectorBox.offsetHeight;
-
-    selectedSpanBucket.clear();
-    allSpans.forEach((span: HTMLSpanElement): void => {
-      span.classList.remove("when-selected");
-      const spanLeft: number = span.offsetLeft + contentArea.offsetLeft;
-      const spanTop: number = span.offsetTop + contentArea.offsetTop;
-      const spanEndWidth: number = span.offsetLeft + span.offsetWidth + contentArea.offsetLeft;
-      const spanEndHeight: number = span.offsetTop + span.offsetHeight + contentArea.offsetTop;
-      if (
-        spanLeft >= startLeft &&
-        spanTop >= startTop &&
-        spanEndWidth <= selectorEndWidth &&
-        spanEndHeight <= selectorEndHeight
-      ) {
-        span.classList.add("when-selected");
-        selectedSpanBucket.add(span);
-      }
-    });
-
-    console.log(selectedSpanBucket);
-
-    // 添加消失动画
-    selectorBox
-      .animate(
-        [
-          {
-            filter: "opacity(1)"
-          },
-          {
-            filter: "opacity(0)"
-          }
-        ],
-        {
-          duration: 100
-        }
-      )
-      .finished.finally((): void => {
-        selectorBox.removeAttribute("style");
-      });
-  });
-
-  function whenMoveAction(moveAction: MouseEvent): void {
+  // 鼠标移动过程中设置框选器的操作
+  const whenMoveAction: (mv: MouseEvent) => void = (moveAction: MouseEvent): void => {
     moveAction.stopPropagation();
     moveAction.stopImmediatePropagation();
     // 定位的初始位置
@@ -156,5 +78,86 @@ window.addEventListener("load", (): void => {
 
     // 修改 style 字符串
     selectorBox.style.cssText = `left:${applyLeft}px;top:${applyTop}px;width:${applyWith}px;height:${applyHeight}px`;
+  };
+
+  // 填充元素
+  const fragmentWrapper: DocumentFragment = document.createDocumentFragment();
+  for (let i: number = 1; i <= 36; ++i) {
+    const span = document.createElement("span");
+    span.classList.add("item");
+    span.textContent = `${i}`;
+    allSpans.push(span);
+    fragmentWrapper.appendChild(span);
   }
+
+  // 设置代理绑定监听器
+  contentArea.addEventListener("click", (ev: MouseEvent): void => {
+    ev.stopPropagation();
+    selectedSpanBucket.clear();
+    if (ev.target !== contentArea) {
+      (ev.target as HTMLSpanElement).classList.toggle("when-selected");
+      selectedSpanBucket.add(ev.target as HTMLSpanElement);
+    }
+  });
+  contentArea.appendChild(fragmentWrapper);
+
+  // 鼠标按下后
+  document.addEventListener("mousedown", (clickEv: MouseEvent): void => {
+    clickEv.stopPropagation();
+
+    positionProxy.x = clickEv.pageX - selectorArea.offsetLeft;
+    positionProxy.y = clickEv.pageY - selectorArea.offsetTop;
+    if (positionProxy.x >= 0 && positionProxy.y >= 0) {
+      document.addEventListener("mousemove", whenMoveAction);
+      selectorBox.classList.remove("hide");
+    }
+  });
+
+  // 鼠标抬起后
+  document.addEventListener("mouseup", (): void => {
+    document.removeEventListener("mousemove", whenMoveAction);
+    const startLeft: number = selectorBox.offsetLeft;
+    const startTop: number = selectorBox.offsetTop;
+    const selectorEndWidth: number = startLeft + selectorBox.offsetWidth;
+    const selectorEndHeight: number = startTop + selectorBox.offsetHeight;
+
+    selectedSpanBucket.clear();
+    for (const span of allSpans) {
+      span.classList.remove("when-selected");
+      const spanLeft: number = span.offsetLeft + contentArea.offsetLeft;
+      const spanTop: number = span.offsetTop + contentArea.offsetTop;
+      const spanEndWidth: number = span.offsetLeft + span.offsetWidth + contentArea.offsetLeft;
+      const spanEndHeight: number = span.offsetTop + span.offsetHeight + contentArea.offsetTop;
+      if (
+        spanLeft >= startLeft &&
+        spanTop >= startTop &&
+        spanEndWidth <= selectorEndWidth &&
+        spanEndHeight <= selectorEndHeight
+      ) {
+        span.classList.add("when-selected");
+        selectedSpanBucket.add(span);
+      }
+    }
+    console.log(selectedSpanBucket);
+
+    // 添加消失动画
+    selectorBox
+      .animate(
+        [
+          {
+            filter: "opacity(1)"
+          },
+          {
+            filter: "opacity(0)"
+          }
+        ],
+        {
+          duration: 120
+        }
+      )
+      .finished.finally((): void => {
+        selectorBox.classList.add("hide");
+        selectorBox.style.cssText = "";
+      });
+  });
 });
