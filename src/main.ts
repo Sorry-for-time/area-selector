d922a02ad62db74f89a97c06d7d83f4b14241cf5
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-12 00:29:16 +0800

    使用 transform 更改拖拽实现

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,22 @@ type PositionType = {
 };
 
 /**
- * 存储位置信息
+ * 拖拽点的起始位置
  */
-const positionProxy = new Proxy(
-  {
-    x: 0,
-    y: 0
-  },
-  {
-    set(target: PositionType, p: string | symbol, newValue: any, receiver: any) {
-      return Reflect.set(target, p, newValue, receiver);
-    },
-    get(target: PositionType, p: string | symbol, receiver: any) {
-      return Reflect.get(target, p, receiver);
-    }
-  }
-);
+const dragStartCoordinates: PositionType = {
+  x: 0,
+  y: 0
+};
+
+/**
+ * 最后一次更新的坐标位置
+ */
+const lastSavedPosition = {
+  translateX: 0,
+  translateY: 0,
+  width: 0,
+  height: 0
+};
 
 window.addEventListener("load", (): void => {
   const selectorArea = document.querySelector(".selector-area") as HTMLDivElement;
@@ -37,8 +37,8 @@ window.addEventListener("load", (): void => {
     moveAction.stopPropagation();
     moveAction.stopImmediatePropagation();
     // 定位的初始位置
-    const initX: number = positionProxy.x;
-    const initY: number = positionProxy.y;
+    const initX: number = dragStartCoordinates.x;
+    const initY: number = dragStartCoordinates.y;
 
     // 移动过程中的相对偏移量
     const movingX: number = moveAction.pageX - selectorArea.offsetLeft;
@@ -52,10 +52,10 @@ window.addEventListener("load", (): void => {
 
     // 正方向拖拽的情况
     if (applyWith >= selectorArea.offsetWidth - initX) {
-      applyWith = selectorArea.offsetWidth - initX - 4; /* 减去边框值*/
+      applyWith = selectorArea.offsetWidth - initX;
     }
     if (applyHeight >= selectorArea.offsetHeight - initY) {
-      applyHeight = selectorArea.offsetHeight - initY - 4; /* 减去边框值 */
+      applyHeight = selectorArea.offsetHeight - initY;
     }
 
     // 往反方向拖拽的情况
@@ -76,52 +76,51 @@ window.addEventListener("load", (): void => {
       }
     }
 
-    // 修改 style 字符串
-    selectorBox.style.cssText = `left:${applyLeft}px;top:${applyTop}px;width:${applyWith}px;height:${applyHeight}px`;
+    // 更新位置
+    lastSavedPosition.translateX = applyLeft;
+    lastSavedPosition.translateY = applyTop;
+    lastSavedPosition.width = applyWith;
+    lastSavedPosition.height = applyHeight;
+
+    // 使用 transform 修改 style 字符串
+    selectorBox.style.cssText = `transform:translate(${applyLeft}px,${applyTop}px) scale(${
+      applyWith / selectorArea.offsetWidth
+    },${applyHeight / selectorArea.offsetHeight})`;
   };
 
   // 填充元素
   const fragmentWrapper: DocumentFragment = document.createDocumentFragment();
   for (let i: number = 1; i <= 36; ++i) {
-    const span = document.createElement("span");
+    const span: HTMLSpanElement = document.createElement("span");
     span.classList.add("item");
     span.textContent = `${i}`;
     allSpans.push(span);
     fragmentWrapper.appendChild(span);
   }
 
-  // 设置代理绑定监听器
-  contentArea.addEventListener("click", (ev: MouseEvent): void => {
-    ev.stopPropagation();
-    selectedSpanBucket.clear();
-    if (ev.target !== contentArea) {
-      (ev.target as HTMLSpanElement).classList.toggle("when-selected");
-      selectedSpanBucket.add(ev.target as HTMLSpanElement);
-    }
-  });
   contentArea.appendChild(fragmentWrapper);
 
   // 鼠标按下后
   document.addEventListener("mousedown", (clickEv: MouseEvent): void => {
     clickEv.stopPropagation();
-
-    positionProxy.x = clickEv.pageX - selectorArea.offsetLeft;
-    positionProxy.y = clickEv.pageY - selectorArea.offsetTop;
-    if (positionProxy.x >= 0 && positionProxy.y >= 0) {
+    dragStartCoordinates.x = clickEv.pageX - selectorArea.offsetLeft;
+    dragStartCoordinates.y = clickEv.pageY - selectorArea.offsetTop;
+    // 在范围内才进行监听 --> 粗暴做法
+    if (dragStartCoordinates.x >= 0 && dragStartCoordinates.y >= 0) {
       document.addEventListener("mousemove", whenMoveAction);
-      selectorBox.classList.remove("hide");
     }
   });
 
   // 鼠标抬起后
   document.addEventListener("mouseup", (): void => {
+    // 释放移动过程监听器
     document.removeEventListener("mousemove", whenMoveAction);
-    const startLeft: number = selectorBox.offsetLeft;
-    const startTop: number = selectorBox.offsetTop;
-    const selectorEndWidth: number = startLeft + selectorBox.offsetWidth;
-    const selectorEndHeight: number = startTop + selectorBox.offsetHeight;
+    const startLeft: number = lastSavedPosition.translateX;
+    const startTop: number = lastSavedPosition.translateY;
+    const selectorEndWidth: number = startLeft + lastSavedPosition.width;
+    const selectorEndHeight: number = startTop + lastSavedPosition.height;
 
-    selectedSpanBucket.clear();
+    selectedSpanBucket.clear(); /* 清理 Set */
     for (const span of allSpans) {
       span.classList.remove("when-selected");
       const spanLeft: number = span.offsetLeft + contentArea.offsetLeft;
@@ -156,8 +155,7 @@ window.addEventListener("load", (): void => {
         }
       )
       .finished.finally((): void => {
-        selectorBox.classList.add("hide");
-        selectorBox.style.cssText = "";
+        selectorBox.style.cssText = "transform:translate(0,0) scale(0,0)";
       });
   });
 });
